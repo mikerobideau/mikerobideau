3ca9e210140b091ae8e3c2efd85261484fb6146a
Mike Robideau <kingrobideau@gmail.com>
2023-02-15 14:02:53 -0500

    redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/march-madness',
+        permanent: true,
+      }
+    ]
+  },
   env: {
     MYSQL_HOST: process.env.MYSQL_HOST,
     MYSQL_DATABASE: process.env.MYSQL_DATABASE,
